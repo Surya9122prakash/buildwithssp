c567be9c4ef9c2e6305391a542365f76a37c4573
Surya9122Prakash <sspsurya2002@gmail.com>
2026-07-01 07:40:49 +0530

    fix: add lazyConnect to Redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,9 +2,12 @@ import Redis from "ioredis";
 
 const globalForRedis = global as unknown as { redis: Redis | undefined };
 
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 export const redis =
   globalForRedis.redis ||
-  new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
+  new Redis(redisUrl, {
+    lazyConnect: true,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForRedis.redis = redis;
